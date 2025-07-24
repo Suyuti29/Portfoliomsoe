ff26dea25a012fdc3d40abbbc9a50048d479d0ec
Nama Kamu <msuyuti7@gmail.com>
2025-07-24 08:52:56 +0700

    new

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,12 +19,12 @@ const Navbar = () => {
   }, []);
 
   return (
-    <div className="navbar py-7 flex items-center justify-between">
+    <div className="navbar py-7 flex items-center justify-between ">
       <div className="logo">
         <h1 className="text-3xl font-bold bg-white text-black p-1 md:bg-transparent md:text-white">Portfolio</h1>
       </div>
       <ul
-        className={`menu flex items-center sm:gap- 10 gap-4 md:static fixed left-1/2 -translate-x-1/2 md:-translate-x-0 md:opacity-100 bg-white/30
+        className={`menu flex items-center sm:gap-10 gap-1 md:static fixed left-1/2 -translate-x-1/2 md:-translate-x-0 md:opacity-100 bg-white/30
        backdrop-blur-md p-4 rounded-br-2xl rounded-bl-2xl md:bg-transparent transition-all md:transition-none z-40 ${active ? "top-0 opacity-100" : "-top-10 opacity-0"}`}
       >
         <li>
@@ -33,7 +33,7 @@ const Navbar = () => {
           </a>
         </li>
         <li>
-          <a href="#tool" className="sm:text-lg text-base font-medium px-2 py-2 rounded hover:bg-violet-600 hover:text-white transition duration-300 block">
+          <a href="#pengalaman" className="sm:text-lg text-base font-medium px-2 py-2 rounded hover:bg-violet-600 hover:text-white transition duration-300 block">
             Pengalaman
           </a>
         </li>
